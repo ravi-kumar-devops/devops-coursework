f80d69123a109a0717b9170614b9fe75a32b0b46
ravi-kumar-devops <R359@caledonian.ac.uk>
2025-02-21 12:59:26 +0000

    Description of changes 1.0.0

--- Dex2Hex.py
+++ Dex2Hex.py
@@ -21,4 +21,4 @@ if __name__ == "__main__":
         except ValueError:
             print("Please provide a valid integer.")
     else:
-        print("Usage: python script.py <decimal_number>")
+        print("Usage: python script.py <decimal_number> ")
